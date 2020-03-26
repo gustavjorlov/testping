629a71dcc3dc7d11a35148996c0c0595e87e804f
Gurra <gustav.jorlov@kits.se>
2020-03-26 19:59:42 +0100

    very simple stuff, printing cwd()

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Watch your tests run and let the OS Nofitication badge ping you when tests fail
+
+Dependencies: `node >= 8`
+
+Create `.testpingrc` and provide your test command to run tests in watch mode.
+
+```javascript
+{
+  "run": "npm test"
+}
+```
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { fork } = require("child_process");
-const minimist = require("minimist");
 const notifier = require("node-notifier");
+const { readRuntimeConfig } = require("./read_config");
 
-module.exports = () => {
-  const args = minimist(process.argv.slice(2));
-  console.log(args);
+module.exports = async () => {
   console.log(process.cwd());
+  try {
+    const runtimeConfig = await readRuntimeConfig();
+  } catch (e) {
+    console.log(e.message, e);
+  }
 };
+
+// ok, make this somehow execute jest or mocha, or whatever the user wants
+// output everything to the console
+// invoke the node-notifier when any test breaks
+
+// done
